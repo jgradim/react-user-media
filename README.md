66afe4afce43c334cbdc60e9f14458ebe724a737
João Gradim <joao.gradim@gmail.com>
2015-12-04 16:33:20 +0000

    Add Webcam component

--- README.md
+++ README.md
@@ -1,4 +1,52 @@
-React User Media
+react-user-media
 ================
 
 Provides components for access to audio and webcam
+
+Instalation
+-----------
+
+```
+npm install react-user-media
+```
+
+```
+import Webcam from "react-user-media";
+```
+
+Webcam
+------
+
+Renders a `video` element that autoplays the feed from the user's webcam (if allowed). If used with a `ref`, it's possible to take screenshots of the video feed to a data url format.
+
+#### Basic usage
+
+```
+<Webcam />
+```
+
+#### Advanced usage (screenshots)
+
+In your component, render `Webcam` along with a `ref`:
+
+```
+<Webcam ref="webcam" />
+```
+
+and then use that ref to access the `captureScreenshot` method. This methods returns a string with a data-url representation of the current frame of the video feed.
+
+```
+this.refs.webcam.captureScreenshot();
+``` 
+
+See the `examples` folder for more in-depth examples.
+
+#### Props
+
+| **Prop**        | **Type** | **Default**   | 
+|-----------------|----------|---------------|
+| `audio`         | `bool`   | `true`        | 
+| `width`         | `number` | `640`         | 
+| `height`        | `number` | `480`         | 
+| `captureFormat` | `string` | `"image/png"` | 
+
--- src/webcam/index.js
+++ src/webcam/index.js
@@ -1,6 +1,156 @@
 import React from "react";
 
 class Webcam extends React.Component {
+  static propTypes = {
+    audio: React.PropTypes.bool,
+    width: React.PropTypes.number,
+    height: React.PropTypes.number,
+    captureFormat: React.PropTypes.oneOf([
+      "image/png",
+      "image/jpeg",
+      "image/webp"
+    ])
+  };
+
+  static defaultProps = {
+    audio: true,
+    width: 640,
+    height: 480,
+    captureFormat: "image/png"
+  };
+
+  static _mediaStream = null;
+  static _captureCanvas = null;
+
+  //---------------------------------------------------------------------------
+  // Initialization
+  //---------------------------------------------------------------------------
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hasUserMedia: false,
+      userMediaRequested: false
+    };
+  }
+
+  //---------------------------------------------------------------------------
+  // Lifecycle methods
+  //---------------------------------------------------------------------------
+
+  componentDidMount() {
+    if (!this._hasGetUserMedia()) {
+      return false;
+    }
+
+    const { hasUserMedia, userMediaRequested } = this.state;
+    if (!hasUserMedia && !userMediaRequested) {
+      this._requestUserMedia();
+    }
+  }
+
+  componentWillUnmount() {
+    this._mediaStream.getTracks().forEach((track) => track.stop());
+  }
+
+  //---------------------------------------------------------------------------
+  // External methods
+  //---------------------------------------------------------------------------
+
+  _hasGetUserMedia() {
+    return !!(
+      navigator.getUserMedia ||
+      navigator.webkitGetUserMedia ||
+      navigator.mozGetUserMedia ||
+      navigator.msGetUserMedia
+    );
+  }
+
+  _requestUserMedia() {
+    navigator.getUserMedia = (
+      navigator.getUserMedia ||
+      navigator.webkitGetUserMedia ||
+      navigator.mozGetUserMedia ||
+      navigator.msGetUserMedia
+    );
+
+    let constraints = {
+      video: true,
+      audio: this.props.audio
+    }
+
+    navigator.getUserMedia(
+      constraints,
+      (stream) => {
+        const video = this.refs.video;
+
+        if (window.URL) {
+          video.src = window.URL.createObjectURL(stream);
+        } else {
+          video.src = stream;
+        }
+
+        this.mediaStream = stream;
+
+        this.setState({
+          hasUserMedia: true,
+          userMediaRequested: true
+        });
+      },
+      (error) => {
+        console.log("An error occured while requesting user media");
+        throw(error);
+      }
+    );
+
+  }
+
+  _getCanvas() {
+    if (this._captureCanvas) {
+      return this._captureCanvas;
+    }
+
+    this._captureCanvas = document.createElement("canvas");
+    this._captureCanvas.width = this.props.width;
+    this._captureCanvas.height = this.props.height;
+  }
+
+  //---------------------------------------------------------------------------
+  // External methods
+  //---------------------------------------------------------------------------
+
+  captureScreenshot() {
+    const { hasUserMedia, userMediaRequested } = this.state;
+    const { width, height, captureFormat } = this.props;
+
+    if (hasUserMedia && userMediaRequested) {
+      const canvas = this._getCanvas();
+      const ctx = canvas.getContext("2d");
+
+      ctx.drawImage(this.refs.video, 0, 0, width, height);
+
+      return canvas.toDataURL(captureFormat);
+    }
+  }
+
+  //---------------------------------------------------------------------------
+  // Render
+  //---------------------------------------------------------------------------
+
+  render() {
+    const { width, height } = this.props;
+
+    return (
+      <video
+        width={width}
+        height={height}
+        ref="video"
+        autoPlay
+      />
+    )
+  }
+
 };
 
 export default Webcam;
